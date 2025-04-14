4295295c80494ea6734b00f110bde4017e946390
jordnlnn <80535966+jordnlnn@users.noreply.github.com>
2025-04-14 08:25:32 -0700

    update javascript so that images are not added to end of container

--- src/script.js
+++ src/script.js
@@ -1,32 +1,34 @@
 let quoteForm = document.querySelector("#quote-generator-form");
 let quoteContainer = document.querySelector(".quote-container");
 let userInput =document.querySelector("#user-input");
+let pokeQuoteContainer = document.querySelector(".pokemon-and-quote-container");
+let pokemonImg = document.querySelector(".pokemon-image");
 
 function displayPokeQuote(response) {
     //TYPEWRITER EFFECT FOR QUOTE
     new Typewriter('#quote', {
         strings: response.data.answer,
-        autoStart: true, delay: 1
+        autoStart: true, delay: 2
     });
 }
 
 function generateQuote(event) {
     //PREVENTS FORM SUBMISSION
     event.preventDefault();
-
     //USER INPUT
     let userPokemon = userInput.value.trim().toLowerCase();
     //BUILD POKEMON URL
     let pokemonApiUrl = `https://pokeapi.co/api/v2/pokemon/${userPokemon}`;
     console.log("User typed:", userPokemon);
 
+
     axios.get(pokemonApiUrl).then(function (response) {
     let spriteUrl = response.data.sprites.front_default;
     console.log("Sprite URL:", spriteUrl);
-
-    let pokemonImg = `<img src="${spriteUrl}" height="250" />`;
+    pokemonImg.src = spriteUrl;
+    //let pokemonImg = `<img src="${spriteUrl}" height="300" />`;
     
-    quoteContainer.insertAdjacentHTML("afterend", pokemonImg);
+    //quoteContainer.insertAdjacentHTML("afterend", pokemonImg);
     });
 
     let aiApiKey = "df0267tdf4o3bfae34b454fb00b472a9";
@@ -36,6 +38,8 @@ function generateQuote(event) {
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${userPrompt}&context=${context}&key=${aiApiKey}`;
 
     axios.get(apiUrl).then(displayPokeQuote);
+
+    pokeQuoteContainer.style.display = "grid"; 
 }
 
 //SUBMIT FORM BUTTON
